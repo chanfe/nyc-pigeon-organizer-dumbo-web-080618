7f78ea8411085ea347f74e7e54193596e7f37110
Felix Chan <felixchan29@gmail.com>
2018-07-23 14:59:41 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,11 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   
   data.each do |att, att_data|
     att_data.each do |var, var_data|
-      
+      binding.pry
     end
   end
 end

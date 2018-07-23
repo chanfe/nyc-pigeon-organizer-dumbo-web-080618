2c4113ef79334fd4feea1653fb761f69d12cc0fe
Felix Chan <felixchan29@gmail.com>
2018-07-23 15:16:51 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,7 +16,13 @@ def nyc_pigeon_organizer(data)
     end
   end
   
-  
+  data.each do |att, att_data|
+    att_data.each do |var, var_data|
+      for i in 0..var_data.length-1
+        new_hash[var_data[i]][att] << var.to_s 
+      end
+    end
+  end
   
   
   

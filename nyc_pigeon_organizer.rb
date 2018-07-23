21df6c8db832ce608049424f5a5cb8f69000a27e
Felix Chan <felixchan29@gmail.com>
2018-07-23 15:12:22 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,19 +2,16 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
+  att_hash = {}
   
   data.each do |att, att_data|
-    att_data.each do |var, var_data|
-      for i in 0..var_data.length-1
-        new_hash[var_data[i]] = {}
-      end
-    end
+    att_hash[att] = []
   end
   
   data.each do |att, att_data|
     att_data.each do |var, var_data|
       for i in 0..var_data.length-1
-        new_hash[var_data[i]] = {}
+        new_hash[var_data[i]] = att_hash
       end
     end
   end
